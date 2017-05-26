22702c2d2ffa75e31298bb1bc9bf7c49c2f35176
dhobbs81 <dhobbs81@gmail.com>
2017-05-26 15:01:09 -0400

    Initial commit of template project including basic HTML, stylesheets, and javascript.

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+/**
+ * The long description of the file's purpose goes here and
+ * describes in detail the complete functionality of the file.
+ * This description can span several lines and ends with a period.
+ *
+ * @summary   A short description of the file.
+ *
+ * http://usejsdoc.org/
+ * @link      URL
+ * @since     x.x.x (if available)
+ * @requires javascriptlibrary.js
+ * @class
+ * @classdesc This is a description of the MyClass class.
+ */
+var helloWorldHeading = document.querySelector('h1');
+helloWorldHeading.textContent = 'Hello world!';
